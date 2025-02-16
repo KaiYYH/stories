1afe9f0507ae9cccf70869bf68220384cd6fe1aa
kai-h-8 <kai.yy.houston@gmail.com>
2025-02-16 12:27:48 -0500

    Story box formatting. Name and description now hide overflow

--- stories/src/app/components/StoryBox.tsx
+++ stories/src/app/components/StoryBox.tsx
@@ -7,11 +7,17 @@ interface Props {
 
 export default function StoryBox(props: Props) {
     return (
-        <div className="border-solid border border-foreground p-3">
-            <h1 className="text-lg">{props.name}</h1>
-            <p>{props.description}</p>
-            <p className="text-gray-400 text-sm">Post Count: {props.postCount}</p>
-            {props.lastPost && (<p className="text-gray-400 text-sm">Last Post: {props.lastPost.toDateString()}</p>)}
+        <div className="border-solid border border-foreground grid grid-rows-6 p-3 h-40">
+            <div className="row-span-1">
+                <h1 className="text-lg line-clamp-1">{props.name}</h1>
+            </div>
+            <div className="row-span-3">
+                <p className="line-clamp-2">{props.description}</p>
+            </div>
+            <div className="row-span-2">
+                <p className="text-gray-400 text-sm">Post Count: {props.postCount}</p>
+                {props.lastPost && (<p className="text-gray-400 text-sm">Last Post: {props.lastPost.toDateString()}</p>)}
+            </div>
         </div>
     )
 }
