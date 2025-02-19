59a3304b79af87b7a451413a5e1e74f7759b2a99
kai-h-8 <kai.yy.houston@gmail.com>
2025-02-18 22:19:47 -0500

    It's on the right now and added filler links so it's not a baby island on the right

--- stories/src/app/components/Navbar.tsx
+++ stories/src/app/components/Navbar.tsx
@@ -4,23 +4,32 @@ export default function NavBar() {
 
     const items = [
         {
-          page: "Home",
+          page: "Story Grid",
           path: "/",
         },
         {
           page: "Story",
           path: "/pages/story",
         },
+        {
+          page: "Explore",
+          path: "/",
+        },
+        {
+          page: "Login",
+          path: "/",
+        },
       ];
 
   return (
-    <div className="container bg-background mt-3 shadow sm:flex-row font-[family-name:var(--font-geist-sans)] grid grid-cols-7">
+    <div className="container bg-background mt-3 shadow sm:flex-row font-[family-name:var(--font-geist-sans)] grid grid-cols-3">
         <div>
             <a href="/">
                 <span className="text-5xl pl-3 text-foreground">STORIES</span>
             </a>
         </div>
-        <div className="grid grid-cols-3 justify-items-center items-center">
+        <div></div>
+        <div className="grid grid-flow-col float-right justify-items-center items-center">
             {items.map((item) => (
                 <NavBarLink
                     pageName={item.page}
