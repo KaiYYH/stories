da755b1bd52185690b60a045e6e484642a59184f
kai-h-8 <kai.yy.houston@gmail.com>
2025-03-02 15:38:00 -0500

    Light mode is light and dark mode is dark

--- stories/src/app/components/Modal.tsx
+++ stories/src/app/components/Modal.tsx
@@ -23,13 +23,11 @@ export const Modal = (
     if(!isOpen) return null;
 
     return (
-        
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center transition-opacity duration-300">
-            <div className="bg-foreground text-background p-5 rounded-md shadow.lg max-w-md md:ax-w-md mx-auto transition-transform duration-300 transform-gpu">
+            <div className="bg-background text-foreground p-5 rounded-md border border-foreground shadow.lg max-w-md md:ax-w-md mx-auto transition-transform duration-300 transform-gpu">
                 <button onClick={onClose} className="float-right hover:text-[#383838] dark:hover:text-[#7a7a7a]">X</button>
                 <div className="mt-8">{children}</div>
             </div>
         </div>
-    
     );
 };
