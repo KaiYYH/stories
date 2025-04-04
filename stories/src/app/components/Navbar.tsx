acb90ed9627c8ad61b9b641010d7a07cbc68e874
Kai <84109424+KaiYYH@users.noreply.github.com>
2025-04-03 23:32:56 -0400

    Update Navbar.tsx

--- stories/src/app/components/Navbar.tsx
+++ stories/src/app/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function NavBar() {
         },
         {
           page: "My Stories",
-          path: "/myStories",
+          path: "/my-stories",
         },
         {
           page: "Login",
