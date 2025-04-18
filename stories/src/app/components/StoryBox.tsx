f54c767b298795e7ef7e7f57f0017a6a3cf75227
kai-h-8 <kai.yy.houston@gmail.com>
2025-04-17 22:02:29 -0400

    Links added to story grid and goes to the story page links

--- stories/src/app/components/StoryBox.tsx
+++ stories/src/app/components/StoryBox.tsx
@@ -1,4 +1,5 @@
 interface Props {
+    storyId: number,
     name: string,
     description: string,
     postCount: number,
@@ -6,10 +7,12 @@ interface Props {
 }
 
 export default function StoryBox(props: Props) {
+
     return (
         <div className="border-solid border border-foreground grid grid-rows-6 p-3 h-40">
-            <div className="row-span-1">
-                <h1 className="text-lg line-clamp-1">{props.name}</h1>
+            <div className="row-span-1 grid grid-cols-12">
+                <div className="col-span-11"><h1 className="text-lg line-clamp-1 no-underline hover:underline"><a href={`/pages/story?id=${props.storyId}`}>{props.name}</a></h1></div>
+                <div className="col-span-1"><p><a className="hover:text-[#707070] dark:hover:[#707070]" href={`/pages/story?id=${props.storyId}`}>&#8594;</a></p></div>
             </div>
             <div className="row-span-3">
                 <p className="line-clamp-2">{props.description}</p>
