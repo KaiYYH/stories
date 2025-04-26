b135345b15683811968c6f418b680649647adb33
kai-h-8 <kai.yy.houston@gmail.com>
2025-04-26 16:52:26 -0400

    Edit story modal works

---
+++ stories/src/app/components/EditStoryModal.tsx
@@ -0,0 +1,42 @@
+import { Modal } from "./Modal";
+
+interface Props {
+    isModalOpen: boolean,
+    onClose: () => void,
+    onSubmit: (name: string, description: string) => void,
+    name: string,
+    description: string,
+}
+
+export default function CreateStoryModal(props: Props) {
+    function handleSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const form = event.currentTarget
+        const formElements = form.elements as typeof form.elements & {
+            nameInput: {value: string}
+            descriptionInput: {value: string}
+        }
+
+        props.onSubmit(formElements.nameInput.value, formElements.descriptionInput.value)
+    }
+    
+    return(
+        <Modal
+            isOpen={props.isModalOpen}
+            onClose={props.onClose}
+        >
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div className="grid grid-cols-[25%_72%] gap-3">
+                        <div className=""><label>Name: </label></div>
+                        <div className=""><input type="text" defaultValue={props.name} maxLength={45} id="nameInput" required className="float-right bg-[#ccc] rounded-md w-64 p-1" /></div>
+                        <div className=""><label>Description: </label></div>
+                        <div className=""><textarea maxLength={200} defaultValue={props.description} id="descriptionInput" className="bg-[#ccc] rounded-md w-64 float-right p-1" /></div>
+                        <div className=""></div>
+                        <div className=""><input type="submit" value="Submit" className="cursor-pointer float-right text-foreground hover:text-[#383838] dark:hover:text-[#7a7a7a] bg-background rounded-md p-2"/></div>
+                    </div>
+                </form>
+            </div>
+        </Modal>
+    )
+}
--- stories/src/app/components/StoryBox.tsx
+++ stories/src/app/components/StoryBox.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { storyPageLink } from "../../constants";
+import EditStoryModal from "./EditStoryModal";
 
 interface Props {
     key: number,
@@ -11,6 +13,24 @@ interface Props {
 
 export default function StoryBox(props: Props) {
     let storyLink = storyPageLink(props.storyId);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    async function onSubmit(name: string, description: string) {
+        await fetch(`https://localhost:7009/api/Stories/${props.storyId}`, {
+            method: "PUT",
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                Name: name,
+                Description: description,
+            }),
+        })
+        .then(response => response.json())
+        .catch(e => {console.log(e)});
+    
+        setIsModalOpen(false)
+        }
 
     return (
         <div className="border-solid border border-foreground grid grid-rows-6 p-3 h-40">
@@ -21,9 +41,28 @@ export default function StoryBox(props: Props) {
             <div className="row-span-3">
                 <p className="line-clamp-2">{props.description}</p>
             </div>
-            <div className="row-span-2">
-                <p className="text-gray-400 text-sm">Post Count: {props.postCount}</p>
-                <p className="text-gray-400 text-sm">Last Post: {props.lastPost?.toDateString() ?? "No posts yet!"}</p>
+            <div className="row-span-2 grid grid-cols-12">
+                <p className="text-gray-400 text-sm col-span-11">Post Count: {props.postCount}</p>
+                <p className="text-gray-400 text-sm col-span-11">Last Post: {props.lastPost?.toDateString() ?? "No posts yet!"}</p>
+                <div className="col-span-1">
+                    <button className="hover:text-[#707070] dark:hover:[#707070]" 
+                        onClick={() => {
+                            setIsModalOpen(true);
+                        }}
+                        >
+                        &#9998;
+                    </button>
+
+                    <EditStoryModal 
+                        isModalOpen={isModalOpen}
+                        onClose={() => {
+                        setIsModalOpen(false);
+                        }}
+                        onSubmit={onSubmit}
+                        name={props.name}
+                        description={props.description}
+                    />
+                </div>
             </div>
         </div>
     )
