e7698b0b22c375e00a13027d10f802d863dee03d
Hbixler <hbixler@unomaha.edu>
2025-06-08 13:28:35 -0700

    login and logout

--- stories/src/app/components/Navbar.tsx
+++ stories/src/app/components/Navbar.tsx
@@ -1,29 +1,38 @@
+"use client"
 import NavBarLink from "./NavBarLink";
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+import Button from "./general/Button";
 
 export default function NavBar() {
+  const { data: session } = useSession();
+  console.log(session);
 
-    const items = [
-        {
-          page: "Home",
-          path: "/",
-        },
-        {
-          page: "Story",
-          path: "/pages/story?id=1",
-        },
-        {
-          page: "My Stories",
-          path: "/my-stories",
-        },
-        {
-          page: "Sign Up",
-          path: "/pages/signup",
-        },
-        {
-          page: "Log In",
-          path: "/pages/login"
-        }
-      ];
+  const items = [
+      {
+        page: "Home",
+        path: "/",
+      },
+      {
+        page: "Story",
+        path: "/pages/story?id=1",
+      },
+      {
+        page: "My Stories",
+        path: "/my-stories",
+      }
+  ];
+
+  if (!session) {
+    items.push({
+      page: "Sign Up",
+      path: "/pages/signup",
+    },
+    {
+      page: "Log In",
+      path: "/pages/login"
+    });
+  }
 
   return (
     <div className="container bg-background mt-3 shadow sm:flex-row font-[family-name:var(--font-geist-sans)] grid grid-cols-3">
@@ -41,6 +50,12 @@ export default function NavBar() {
                     key={item.page}
                 />
             ))}
+            {session && <Button 
+              text="Sign Out" 
+              onClick={async () => {
+                await signOut();
+              }}>
+            </Button>}
         </div>
     </div>
   );
--- stories/src/app/layout.tsx
+++ stories/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { SessionProvider } from "next-auth/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,13 +27,15 @@ export default function RootLayout(
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-          <Navbar />
-        {children}
-      </body>
-    </html>
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+            <Navbar />
+          {children}
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
