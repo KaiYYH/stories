7ad1c980dd1b143b307e361db97250121ffbb81f
Hbixler <67593814+Hbixler@users.noreply.github.com>
2025-08-31 16:23:22 -0500

    Update page.tsx

--- stories/src/app/pages/story/page.tsx
+++ stories/src/app/pages/story/page.tsx
@@ -23,8 +23,6 @@ export default function StoryPage() {
     getStory();
   }, []);
 
-
-
   return (
     <div className="grid items-start justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="grid items-center sm:items-start">
